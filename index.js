d2d4b32b3d801236033beed4f3590c29881fef8e
bradleyphelps4 <73716420+bradleyphelps4@users.noreply.github.com>
2021-04-22 15:21:50 -0400

    Created quote app

---
+++ index.js
@@ -0,0 +1,3 @@
+var quotations = ["Context and memory play powerful roles in all the truly great meals in one's life.","I'm not afraid to look like a big, hairy, smelly, foreign devil in Tokyo, though I do my best not to, I really do.","I'm not afraid to look like an idiot."]
+var index = Math.floor(Math.random() * 3) 
+console.log(quotations[index]);
